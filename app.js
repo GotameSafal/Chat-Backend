7485427cd5ef4eb1cd2f162991300cafd6c0c4d9
GotameSafal <98635951+GotameSafal@users.noreply.github.com>
2023-12-18 10:48:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,8 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [`${process.env.FRONTEND_URI}`],
-    credentials: true,
+    origin: "*";
   })
 );
 app.use("/api/chat", chatRoute);
